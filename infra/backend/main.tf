2c555510f26a12dd6239372d4c1c44a48d0322e7
labakie <95135910+labakie@users.noreply.github.com>
2025-04-10 20:24:42 +0700

    Add terraform files

---
+++ infra/backend/main.tf
@@ -0,0 +1,160 @@
+# create dynamodb table
+resource "aws_dynamodb_table" "visitor_counter" {
+  name         = "VisitorCounterTable-1"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+# create dynamodb table item
+resource "aws_dynamodb_table_item" "table_item" {
+  table_name = aws_dynamodb_table.visitor_counter.name
+  hash_key   = aws_dynamodb_table.visitor_counter.hash_key
+
+  item = <<ITEM
+{
+  "id": {"S": "counter"},
+  "visitor": {"N": "1"}
+}
+ITEM
+
+  # enable after initial apply to prevent visitor counts going back to 1
+  # lifecycle {
+  #   ignore_changes = [
+  #     item
+  #   ]
+  # }
+}
+
+# get current aws region
+data "aws_region" "current" {}
+
+# get current account id
+data "aws_caller_identity" "current" {}
+
+# create policy
+resource "aws_iam_policy" "custom_policy" {
+  name        = "LambdaAccessVisitorCounterTable-1"
+  description = "Policy for get, put, update item in visitor counter table at DynamoDB and enable log."
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "Statement1",
+        "Effect" : "Allow",
+        "Action" : [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:UpdateItem"
+        ],
+        "Resource" : [
+          "arn:aws:dynamodb:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:table/${aws_dynamodb_table.visitor_counter.name}"
+        ]
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : "logs:CreateLogGroup",
+        "Resource" : "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        "Resource" : [
+          "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/VisitorCounterFunction:*"
+        ]
+      }
+    ]
+  })
+}
+
+# create role
+resource "aws_iam_role" "custom_role" {
+  name        = "VisitorCounterRole-1"
+  description = "Allows Lambda functions to call DynamoDB on your behalf."
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+# assign created policy to role
+resource "aws_iam_role_policy_attachment" "custom_attachment" {
+  role       = aws_iam_role.custom_role.name
+  policy_arn = aws_iam_policy.custom_policy.arn
+}
+
+# archive python file to zip
+data "archive_file" "python" {
+  type        = "zip"
+  source_file = "${path.module}/function/lambda_function.py"
+  output_path = "${path.module}/function/lambda_function.zip"
+}
+
+# create lambda function
+resource "aws_lambda_function" "python_lambda" {
+  filename      = "${path.module}/function/lambda_function.zip"
+  function_name = "VisitorCounterFunction-1"
+  role          = aws_iam_role.custom_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.12"
+}
+
+# create HTTP API
+resource "aws_apigatewayv2_api" "http_api" {
+  name          = "VisitorCounterAPI-1"
+  protocol_type = "HTTP"
+}
+
+# integrate lambda function with HTTP API
+resource "aws_apigatewayv2_integration" "lambda_apigateway" {
+  api_id           = aws_apigatewayv2_api.http_api.id
+  integration_type = "AWS_PROXY"
+  description      = "Lambda integration with HTTP API"
+  integration_uri  = aws_lambda_function.python_lambda.invoke_arn
+}
+
+# create auto deploy dev stage 
+resource "aws_apigatewayv2_stage" "dev_stage" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "dev"
+  auto_deploy = true
+}
+
+# set route with GET method
+resource "aws_apigatewayv2_route" "custom_route" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "GET /visitor-counter"
+  target = "integrations/${aws_apigatewayv2_integration.lambda_apigateway.id}"
+}
+
+# attach permission for HTTP API to execute lambda function
+resource "aws_lambda_permission" "lambda_apigateway_permission" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.python_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  statement_id  = "AllowExecuteLambda-1"
+  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
+}
+
+# show API Gateway invoke URL in terminal
+output "api_invoke_url" {
+  value = "${aws_apigatewayv2_stage.dev_stage.invoke_url}/visitor-counter"
+}
---
+++ infra/frontend/main.tf
@@ -0,0 +1,146 @@
+# create bucket
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "bucket-terraform-zar"
+}
+
+# enable all public access
+resource "aws_s3_bucket_public_access_block" "enable_public" {
+  bucket = aws_s3_bucket.test_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# upload html file
+resource "aws_s3_object" "index_html" {
+  bucket       = aws_s3_bucket.test_bucket.id
+  key          = "index.html"
+  source       = "static-site/index.html"
+#  source       = "../static-site/index.html"
+  content_type = "text/html"
+}
+
+# upload css file
+resource "aws_s3_object" "style_css" {
+  bucket       = aws_s3_bucket.test_bucket.id
+  key          = "style.css"
+  source       = "static-site/style.css"
+  content_type = "text/css"
+}
+
+# upload js file
+resource "aws_s3_object" "script_js" {
+  bucket       = aws_s3_bucket.test_bucket.id
+  key          = "script.js"
+  source       = "static-site/script.js"
+  content_type = "application/javascript"
+}
+
+# upload all images
+resource "aws_s3_object" "upload_all_images" {
+  bucket       = aws_s3_bucket.test_bucket.id
+  for_each     = fileset("static-site/img", "*")
+  key          = "img/${each.value}"
+  source       = "static-site/img/${each.value}"
+  content_type = "image/jpeg"
+}
+
+# create OAC resource to securely access S3 bucket
+resource "aws_cloudfront_origin_access_control" "oac_s3" {
+  name                              = aws_s3_bucket.test_bucket.bucket_regional_domain_name
+  description                       = "For cloudfront OAC with S3"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+# get SSL/TLS certificate data for domain from AWS ACM
+data "aws_acm_certificate" "issued" {
+  domain   = "*.zaril.my.id"
+  statuses = ["ISSUED"]
+}
+
+# define local value for S3 origin
+locals {
+  s3_origin_id = "crc-S3-Origin"
+}
+
+# create cloudfront distribution
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.test_bucket.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac_s3.id
+    origin_id                = local.s3_origin_id
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = false
+  default_root_object = "index.html"
+
+  aliases = ["cv.zaril.my.id"]
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 1
+    default_ttl            = 86400
+    max_ttl                = 31536000
+  }
+
+  price_class = "PriceClass_200"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = data.aws_acm_certificate.issued.arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
+  }
+}
+
+# create and attach policy for S3 bucket that allows cloudfront to access 
+resource "aws_s3_bucket_policy" "allow_access_cloudfront_to_s3" {
+  bucket = aws_s3_bucket.test_bucket.id
+  policy = jsonencode({
+    "Version" : "2008-10-17",
+    "Id" : "PolicyForCloudFrontPrivateContent",
+    "Statement" : [
+      {
+        "Sid" : "AllowCloudFrontServicePrincipal",
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "cloudfront.amazonaws.com"
+        },
+        "Action" : "s3:GetObject",
+        "Resource" : "arn:aws:s3:::${aws_s3_bucket.test_bucket.id}/*",
+        "Condition" : {
+          "StringEquals" : {
+            "AWS:SourceArn" : "${aws_cloudfront_distribution.s3_distribution.arn}"
+          }
+        }
+      }
+    ]
+  })
+}
+
+# show cloudfont distribution url
+output "distribution_url" {
+  value = aws_cloudfront_distribution.s3_distribution.domain_name
+}
