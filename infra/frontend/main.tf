b1500d38af5ebd1ac805cc9b4787ace7f3340b26
labakie <95135910+labakie@users.noreply.github.com>
2025-04-11 21:14:56 +0700

    Change value of some attributes

--- infra/frontend/main.tf
+++ infra/frontend/main.tf
@@ -1,11 +1,11 @@
 # create bucket
-resource "aws_s3_bucket" "test_bucket" {
-  bucket = "bucket-terraform-zar"
+resource "aws_s3_bucket" "site_bucket" {
+  bucket = "bucket-crc-znr"
 }
 
-# enable all public access
-resource "aws_s3_bucket_public_access_block" "enable_public" {
-  bucket = aws_s3_bucket.test_bucket.id
+# enable block public access
+resource "aws_s3_bucket_public_access_block" "block_public" {
+  bucket = aws_s3_bucket.site_bucket.id
 
   block_public_acls       = true
   block_public_policy     = true
@@ -15,41 +15,40 @@ resource "aws_s3_bucket_public_access_block" "enable_public" {
 
 # upload html file
 resource "aws_s3_object" "index_html" {
-  bucket       = aws_s3_bucket.test_bucket.id
+  bucket       = aws_s3_bucket.site_bucket.id
   key          = "index.html"
-  source       = "static-site/index.html"
-#  source       = "../static-site/index.html"
+  source       = "../../static-site/index.html"
   content_type = "text/html"
 }
 
 # upload css file
 resource "aws_s3_object" "style_css" {
-  bucket       = aws_s3_bucket.test_bucket.id
+  bucket       = aws_s3_bucket.site_bucket.id
   key          = "style.css"
-  source       = "static-site/style.css"
+  source       = "../../static-site/style.css"
   content_type = "text/css"
 }
 
 # upload js file
 resource "aws_s3_object" "script_js" {
-  bucket       = aws_s3_bucket.test_bucket.id
+  bucket       = aws_s3_bucket.site_bucket.id
   key          = "script.js"
-  source       = "static-site/script.js"
+  source       = "../../static-site/script.js"
   content_type = "application/javascript"
 }
 
 # upload all images
 resource "aws_s3_object" "upload_all_images" {
-  bucket       = aws_s3_bucket.test_bucket.id
-  for_each     = fileset("static-site/img", "*")
+  bucket       = aws_s3_bucket.site_bucket.id
+  for_each     = fileset("../../static-site/img", "*")
   key          = "img/${each.value}"
-  source       = "static-site/img/${each.value}"
+  source       = "../../static-site/img/${each.value}"
   content_type = "image/jpeg"
 }
 
 # create OAC resource to securely access S3 bucket
 resource "aws_cloudfront_origin_access_control" "oac_s3" {
-  name                              = aws_s3_bucket.test_bucket.bucket_regional_domain_name
+  name                              = aws_s3_bucket.site_bucket.bucket_regional_domain_name
   description                       = "For cloudfront OAC with S3"
   origin_access_control_origin_type = "s3"
   signing_behavior                  = "always"
@@ -70,7 +69,7 @@ locals {
 # create cloudfront distribution
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
-    domain_name              = aws_s3_bucket.test_bucket.bucket_regional_domain_name
+    domain_name              = aws_s3_bucket.site_bucket.bucket_regional_domain_name
     origin_access_control_id = aws_cloudfront_origin_access_control.oac_s3.id
     origin_id                = local.s3_origin_id
   }
@@ -117,7 +116,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
 # create and attach policy for S3 bucket that allows cloudfront to access 
 resource "aws_s3_bucket_policy" "allow_access_cloudfront_to_s3" {
-  bucket = aws_s3_bucket.test_bucket.id
+  bucket = aws_s3_bucket.site_bucket.id
   policy = jsonencode({
     "Version" : "2008-10-17",
     "Id" : "PolicyForCloudFrontPrivateContent",
@@ -129,7 +128,7 @@ resource "aws_s3_bucket_policy" "allow_access_cloudfront_to_s3" {
           "Service" : "cloudfront.amazonaws.com"
         },
         "Action" : "s3:GetObject",
-        "Resource" : "arn:aws:s3:::${aws_s3_bucket.test_bucket.id}/*",
+        "Resource" : "arn:aws:s3:::${aws_s3_bucket.site_bucket.id}/*",
         "Condition" : {
           "StringEquals" : {
             "AWS:SourceArn" : "${aws_cloudfront_distribution.s3_distribution.arn}"
